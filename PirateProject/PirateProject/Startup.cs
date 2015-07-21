b1d7ee3056f78d0f9f97e2c879ffff3b917e0a00
Richard Miller <rmiller2@catalystitservices.com>
2015-07-21 11:39:33 -0700

    Initial Pirate Commit

---
+++ PirateProject/PirateProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PirateProject.Startup))]
+namespace PirateProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
